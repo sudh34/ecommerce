b8621943c6d376c47ba66eb929183939b19d3611
Sudhaim <sudhaimfl@gmail.com>
2026-01-07 12:00:18 +0530

    Use Railway MySQL via env variables

--- models/db.js
+++ models/db.js
@@ -1,13 +1,17 @@
 // models/db.js
 const mysql = require('mysql2/promise');
 
-const connection = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: '1234',
-  database: 'ecommerce',
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-module.exports = connection;
+module.exports = pool;
